fb4cf4b4baeeb51fbfa8b881363f709e7a21cd38
Praveenreddy <praveen@Praveenreddys-MacBook-Pro.local>
2024-03-01 16:38:30 +0000

    master branch

---
+++ src/main/java/com/jks/foodapp/OnlineFoodOrderAppApplication.java
@@ -0,0 +1,13 @@
+package com.jks.foodapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineFoodOrderAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jks/foodapp/OnlineFoodOrderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jks.foodapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFoodOrderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
